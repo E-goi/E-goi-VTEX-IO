9295b2ac99e63267f5954f164f407307721d6641
Luis Araujo <laraujo@e-goi.com>
2025-05-08 16:46:08 +0100

    improv/BB-26846: minor fixes

--- node/resolvers/goidiniOrderSyncBulk.ts
+++ node/resolvers/goidiniOrderSyncBulk.ts
@@ -1,4 +1,5 @@
 import type { ServiceContext } from '@vtex/api'
+
 import type { Clients } from '../clients'
 import type { GoidiniResponse } from '../typings/goidiniResponse'
 
@@ -13,15 +14,16 @@ export async function goidiniOrderSyncBulk(
 
   // pega as credenciais do app
   const appId = process.env.VTEX_APP_ID
+
   if (!appId) throw new Error('No appId defined')
   const settings = await apps.getAppSettings(appId)
 
   try {
     return await goidiniOrderBulkSync.triggerBulk(
-      settings.apikey,     // 1️⃣ apikey
-      settings.appKey,     // 2️⃣ appKey
-      settings.appToken,   // 3️⃣ appToken
-      args.input           // 4️⃣ flag boolean
+      settings.apikey,
+      settings.appKey,
+      settings.appToken,
+      args.input
     )
   } catch (error) {
     throw new Error(error.message)
